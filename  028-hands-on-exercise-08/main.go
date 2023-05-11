a8753c1c72313f1f5db714eb2b0718731ae3c3a9
mojafa <jmohsa@gmail.com>
2023-05-11 22:22:05 +0300

    uint8 and int8 largest possible values

---  028-hands-on-exercise-08/main.go
+++  028-hands-on-exercise-08/main.go
@@ -1,5 +1,22 @@
 package main
 
+import "fmt"
+
 func main() {
+	// Largest int8
+	var largestInt8 int8 = 127
+	fmt.Println("Largest int8:", largestInt8)
+
+	// Largest uint8
+	var largestUint8 uint8 = 255
+	fmt.Println("Largest uint8:", largestUint8)
+
+	// fmt.Printf("%v is of type %T\n", largestInt8, largestInt8)
+	// fmt.Printf("%v is of type %T\n", largestUint8, largestUint8)
 
 }
+
+// write a program that declares two variables
+//
+// one variable to store a VALUE of TYPE int8 and assign to it the largest number possible, then print it
+// one variable to store a VALUE of TYPE uint8 and assign to it the largest number possible, then print it
