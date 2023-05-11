6fc51a369acd010d29049719d6fcfdf95ced361c
mojafa <jmohsa@gmail.com>
2023-05-11 22:14:10 +0300

    added the hash symbol to print the Ox in hex

--- 027-hands-on-exercise-07/main.go
+++ 027-hands-on-exercise-07/main.go
@@ -7,21 +7,21 @@ func main() {
 	fmt.Printf("747 as decimal, %d \n", a)
 	fmt.Printf("747 as binary, %b \n", a)
 	fmt.Printf("747 as octal, %o \n", a)
-	fmt.Printf("747 as hexadecimal, %x \n", a)
+	fmt.Printf("747 as hexadecimal, %#x \n", a)
 	fmt.Println()
 
 	var b int = 911
 	fmt.Printf("911 as decimal, %d \n", b)
 	fmt.Printf("911 as binary, %b \n", b)
 	fmt.Printf("911 as octal, %o \n", b)
-	fmt.Printf("911 as hexadecimal, %x \n", b)
+	fmt.Printf("911 as hexadecimal, %#x \n", b)
 
 	fmt.Println()
 	var c int = 90210
 	fmt.Printf("90210 as decimal, %d \n", c)
 	fmt.Printf("90210 as binary, %b \n", c)
 	fmt.Printf("90210 as octal, %o \n", c)
-	fmt.Printf("90210 as hexadecimal, %x \n", c)
+	fmt.Printf("90210 as hexadecimal, %#x \n", c)
 
 }
 
