82067e1d45a5b2f50625eb5e40778297b354b843
mojafa <jmohsa@gmail.com>
2023-05-15 23:24:46 +0300

    silenced some erroes

--- typecasting/type.go
+++ typecasting/type.go
@@ -1,38 +1,38 @@
 package main
 
-import (
-	"fmt"
-)
+// import (
+// 	"fmt"
+// )
 
 func main() {
-	const name, age = "Kim", 22
-	fmt.Printf("%s is %d years old.\n", name, age)
+	// 	const name, age = "Kim", 22
+	// 	fmt.Printf("%s is %d years old.\n", name, age)
 
-	// It is conventional not to worry about any
-	// error returned by Printf.
+	// 	// It is conventional not to worry about any
+	// 	// error returned by Printf.
 
-	// use printf to print the VALUE and TYPE for each of these variables
+	// 	// use printf to print the VALUE and TYPE for each of these variables
 
-	a := 42
-	b := 42.04
-	var c uint8 = 255
-	var d int8 = -128
-	var e uint = 9223372036854775807
+	// 	a := 42
+	// 	b := 42.04
+	// 	var c uint8 = 255
+	// 	var d int8 = -128
+	// 	var e uint = 9223372036854775807
 
-	f := []int{7, 42, 365, 1024}
+	// 	f := []int{7, 42, 365, 1024}
 
-	g := map[string]int{
-		"James":      42,
-		"Moneypenny": 32,
-	}
+	// 	g := map[string]int{
+	// 		"James":      42,
+	// 		"Moneypenny": 32,
+	// 	}
 
-	type person struct {
-		first string
-		age   int
-	}
+	// 	type person struct {
+	// 		first string
+	// 		age   int
+	// 	}
 
-	h := person{
-		first: "Todd",
-		age:   45,
-	}
+	//	h := person{
+	//		first: "Todd",
+	//		age:   45,
+	//	}
 }
