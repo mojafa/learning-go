e993687c19c9e2713402f7bf89fc2c42d467ba35
mojafa <jmohsa@gmail.com>
2023-05-15 23:30:51 +0300

    working with dependecy management

---
+++ 056-hands-on-exercise-13/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/GoesToEleven/puppy"
+)
+
+func main() {
+	fmt.Println("Hello gophers")
+	s1 := puppy.Bark()
+	s2 := puppy.Barks()
+	s3 := puppy.BigBark()
+	s4 := puppy.BigBarks()
+
+	fmt.Println(s1)
+	fmt.Println(s2)
+	fmt.Println(s3)
+	fmt.Println(s4)
+	// puppy.From13()
+
+}
