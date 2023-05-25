e8ff53125ddee8c3cf4feb58c4146d00d0b39409
mojafa <jmohsa@gmail.com>
2023-05-25 18:28:03 +0300

    ex 27

--- 082-hands-on-exercise-27/main.go
+++ 082-hands-on-exercise-27/main.go
@@ -1,5 +1,21 @@
 package main
 
+import "fmt"
+
+/*
+create a for loop that uses break statement
+● increment or decrement the variable being checked as a condition in the loop
+*/
 func main() {
-	
+	y := 20
+	for {
+		if y > 0 {
+			break
+		}
+
+		// fmt.Printf("y is %v \t\t  \n", y)
+		fmt.Println(y)
+		y--
+
+	}
 }
