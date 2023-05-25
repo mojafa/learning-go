9bec714f157ff8c36a96461782e16d87abbb9343
mojafa <jmohsa@gmail.com>
2023-05-25 18:28:59 +0300

    ex 33

--- 088-hands-on-exercise-33/main.go
+++ 088-hands-on-exercise-33/main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+)
+
 func main() {
-	
+
+	c := 1
+	for i := 0; i < 100; i++ {
+		if x := rand.Intn(5); x == 3 {
+			fmt.Printf("iteration %v \t total count %v \t  x is  %v\n", i, c, x)
+			c++
+		}
+	}
+
 }
+
+/*
+use the "statement statement" idiom to
+○ initialize x with and random int between 0 inclusive and 5 exclusive ○ ifxis3
+■ print "x is 3"
+● run that code 100 times
+● what's the benefit of using the "statement statement" idiom?
+
+*/
