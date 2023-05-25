302ecb6aa103143fb4cbcbcceb7894d689eb4831
mojafa <jmohsa@gmail.com>
2023-05-25 18:27:30 +0300

    ex 23

--- 078-hands-on-exercise-23/main.go
+++ 078-hands-on-exercise-23/main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+)
+
 func main() {
-	
+	x := rand.Intn(10)
+	y := rand.Intn(10)
+
+	switch {
+	case x < 4 && y < 4:
+		fmt.Printf("x=%v and y=%v are both less than 4\n", x, y)
+
+	case x > 6 && y > 6:
+		fmt.Printf("x=%v and y=%v are both greater than 6\n", x, y)
+	case x >= 4 && x <= 6:
+		fmt.Printf("x=%v is greater than or equal to 4 and less than or equal to 6\n", x)
+	case y != 5:
+		fmt.Printf("y=%v is not equal to 5\n", y)
+	default:
+		fmt.Printf("none of the previous cases were met")
+	}
 }
+
+/*
+● Modify the previous program to use a switch statement
+*/
