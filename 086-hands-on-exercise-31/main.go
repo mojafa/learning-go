415e63db18fa542402e1206be1cb620f7a76e9ff
mojafa <jmohsa@gmail.com>
2023-05-25 18:28:44 +0300

    ex 31

--- 086-hands-on-exercise-31/main.go
+++ 086-hands-on-exercise-31/main.go
@@ -1,5 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+
+	m := map[string]int{
+		"James":      42,
+		"Moneypenny": 43,
+	}
+	for k, v := range m {
+		fmt.Printf("key %v \t value %v\n", k, v)
+	}
+
 }
