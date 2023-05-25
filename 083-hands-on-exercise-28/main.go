922df497818ea552511f4b84f045fe153e3984c3
mojafa <jmohsa@gmail.com>
2023-05-25 18:28:12 +0300

    ex 28

--- 083-hands-on-exercise-28/main.go
+++ 083-hands-on-exercise-28/main.go
@@ -1,5 +1,20 @@
 package main
 
+import "fmt"
+
+/*
+use modulus and the continue statement in a loop to print all ODD numbers
+● joke about the programmer and infinite loops
+*/
 func main() {
-	
+	for i := 0; i < 14; i++ {
+		// if i%2 == 1 {
+		// 	fmt.Println(i)
+		// 	continue
+		// }
+		if i%2 != 0 {
+			fmt.Println(i)
+			continue
+		}
+	}
 }
