4a1dfb63409b49264006baa76abe3ca4b6932763
mojafa <jmohsa@gmail.com>
2023-05-25 18:27:53 +0300

    ex 26

--- 081-hands-on-exercise-26/main.go
+++ 081-hands-on-exercise-26/main.go
@@ -1,5 +1,25 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	//create a for loop using only a condition
+	y := 20
+	// for {
+	// 	fmt.Printf("y is %v \t\t  \n", y)
+	// 	if y < 2 {
+	// 		break
+	// 	}
+	// 	y--
+	// }
+
+	for y > 0 {
+		fmt.Printf("y is %v \t\t  \n", y)
+		y--
+	}
 }
+
+/*
+create a for loop using only a condition
+● increment or decrement the variable being checked in the condition
+*/
