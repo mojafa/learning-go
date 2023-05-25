bac39dcef3952437bf963bee375fb05e81706a3c
mojafa <jmohsa@gmail.com>
2023-05-25 18:28:22 +0300

    ex 29

--- 084-hands-on-exercise-29/main.go
+++ 084-hands-on-exercise-29/main.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
+/*
+create a loop that runs 5 times
+● nest a loop within the first loop that also prints 5 times
+● print something in each loop to illustrate what is occuring
+*/
 func main() {
-	
+
+	for i := 0; i < 5; i++ {
+		fmt.Println("--")
+		for j := 0; j < 5; j++ {
+			fmt.Printf("outer loop %v \t inner loop %v\n", i, j)
+		}
+	}
 }
