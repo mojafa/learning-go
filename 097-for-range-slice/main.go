d614e41856d51b8d19d1264baf9112978b49e5c1
mojafa <jmohsa@gmail.com>
2023-06-14 23:39:49 +0300

    slices using for range

--- 097-for-range-slice/main.go
+++ 097-for-range-slice/main.go
@@ -5,4 +5,19 @@ import "fmt"
 func main() {
 	slice := []string{"hello", "world"}
 	fmt.Println(slice)
+	fmt.Printf("%T\n", slice)
+
+	for i, v := range slice {
+		fmt.Printf("%v - %v\n", i, v)
+	}
+	//i index
+	// using blank identifier to not use a returned value
+	for _, v := range slice {
+		fmt.Printf(" %v\n", v)
+	}
+
+	//accessing valies by index position
+	fmt.Println(slice[0])
+	fmt.Println(slice[1])
+
 }
