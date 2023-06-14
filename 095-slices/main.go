11ac27019db543a0cb175b59703fd3a22a4c5886
mojafa <jmohsa@gmail.com>
2023-06-14 23:08:16 +0300

    slices

--- 095-slices/main.go
+++ 095-slices/main.go
@@ -6,3 +6,14 @@ func main() {
 	slice := []string{"hello", "world"}
 	fmt.Println(slice)
 }
+
+// slices is a data dtructure with three elements:s:
+// -length
+// -cap
+// - pointer to an underlying array
+
+// type slice struct {
+// 	array unsafe.Pointer
+// 	len int
+// 	cap int
+// }
