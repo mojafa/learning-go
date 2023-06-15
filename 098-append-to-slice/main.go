ca50e634ee080b548d83d0eb6500a1d5d9cf038a
mojafa <jmohsa@gmail.com>
2023-06-15 14:24:54 +0300

    appending slices

--- 098-append-to-slice/main.go
+++ 098-append-to-slice/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	xi := []int{1, 2, 3, 4}
+	fmt.Println(xi)
+	fmt.Println("-------------")
+	//variadic parameter
+	xi = append(xi, 5, 6, 7, 8, 9, 10)
+
+	fmt.Println(xi)
+	fmt.Println("-------------")
 }
