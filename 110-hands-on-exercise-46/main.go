f48906150d662f960596597645aad6913d3ee6eb
mojafa <jmohsa@gmail.com>
2023-06-18 14:18:02 +0300

    deleting from a slice

--- 110-hands-on-exercise-46/main.go
+++ 110-hands-on-exercise-46/main.go
@@ -1,5 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
+	xi := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+	xi = append(xi[:3], xi[6:]...)
+	fmt.Println(xi)
 
 }
+
+/*
+To DELETE from a slice, we use APPEND along with SLICING. For this hands-on exercise, follow these steps:
+start with this slice
+x := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+use APPEND & SLICING to get these values here which you should ASSIGN to the variable “x” and then print:
+[42, 43, 44, 48, 49, 50, 51]
+
+*/
