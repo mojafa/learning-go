4a76afb98ecc5d82edefba8d81a8a4dd783c1e1f
mojafa <jmohsa@gmail.com>
2023-06-18 19:24:35 +0300

    ex51

---
+++ 120-hands-on-exercise-51/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	//first string is key, second is value
+	m := make(map[string][]string)
+	m[`bond_james`] = []string{`Shaken, not stirred`, `Martinis`, `Women`}
+	m[`moneypenny_miss`] = []string{`James Bond`, `Literature`, `Computer Science`}
+	m[`no_dr`] = []string{`Being evil`, `Ice cream`, `Sunsets`}
+	m[`fleming_ian`] = []string{`steaks`, `cigars`, `espionage`}
+	fmt.Printf("%#v\n", m)
+
+	for k, v := range m {
+		fmt.Println(k)
+		for i, v2 := range v {
+			fmt.Println(i, v2)
+		}
+	}
+	fmt.Println("------Deleting------")
+	delete(m, `fleming_ian`)
+	fmt.Println("---------Record deleted-------")
+
+	for k, v := range m {
+		fmt.Println(k)
+		for i, v2 := range v {
+			fmt.Println(i, v2)
+		}
+	}
+}
