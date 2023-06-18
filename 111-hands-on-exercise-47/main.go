0e282e3aa52c79db455270cfa097c36dd627cbc3
mojafa <jmohsa@gmail.com>
2023-06-18 14:34:36 +0300

    make and append

--- 111-hands-on-exercise-47/main.go
+++ 111-hands-on-exercise-47/main.go
@@ -3,14 +3,45 @@ package main
 import "fmt"
 
 func main() {
-	slice := []string{` Alabama`, ` Alaska`, ` Arizona`, ` Arkansas`, ` California`, ` Colorado`, ` Connecticut`, ` Delaware`, ` Florida`, ` Georgia`, ` Hawaii`, ` Idaho`, ` Illinois`, ` Indiana`, ` Iowa`, ` Kansas`, ` Kentucky`, ` Louisiana`, ` Maine`, ` Maryland`, ` Massachusetts`, ` Michigan`, ` Minnesota`, ` Mississippi`, ` Missouri`, ` Montana`, ` Nebraska`, ` Nevada`, ` New Hampshire`, ` New Jersey`, ` New Mexico`, ` New York`, ` North Carolina`, ` North Dakota`, ` Ohio`, ` Oklahoma`, ` Oregon`, ` Pennsylvania`, ` Rhode Island`, ` South Carolina`, ` South Dakota`, ` Tennessee`, ` Texas`, ` Utah`, ` Vermont`, ` Virginia`, ` Washington`, ` West Virginia`, ` Wisconsin`, ` Wyoming`}
-	fmt.Println(slice)
-	fmt.Println(len(slice))
-	fmt.Println(cap(slice))
-	fmt.Printf("%T\n", slice)
+	x1 := make([]int, 50)
+	fmt.Println(x1)
+	fmt.Println(len(x1))
+	fmt.Println(cap(x1))
+
+	x2 := make([]int, 0, 50)
+	fmt.Println(x2)
+	fmt.Println(len(x2))
+	fmt.Println(cap(x2))
+
+	fmt.Println("---------------------------")
+	//x1 = append(x1, 98)
+	x1[0] = 98
+	x2 = append(x2, 99)
+	fmt.Println(x1)
+	fmt.Println(len(x1))
+	fmt.Println(cap(x1))
+
+	fmt.Println(x2)
+	fmt.Println(len(x2))
+	fmt.Println(cap(x2))
+
+	fmt.Println("---------------------------")
+
+	xs := make([]string, 0, 50)
+	fmt.Println(len(xs))
+	fmt.Println(cap(xs))
+
+	fmt.Println("---------------------------")
+	xs = append(xs, ` Alabama`, ` Alaska`, ` Arizona`, ` Arkansas`, ` California`, ` Colorado`, ` Connecticut`, ` Delaware`, ` Florida`, ` Georgia`, ` Hawaii`, ` Idaho`, ` Illinois`, ` Indiana`, ` Iowa`, ` Kansas`, ` Kentucky`, ` Louisiana`, ` Maine`, ` Maryland`, ` Massachusetts`, ` Michigan`, ` Minnesota`, ` Mississippi`, ` Missouri`, ` Montana`, ` Nebraska`, ` Nevada`, ` New Hampshire`, ` New Jersey`, ` New Mexico`, ` New York`, ` North Carolina`, ` North Dakota`, ` Ohio`, ` Oklahoma`, ` Oregon`, ` Pennsylvania`, ` Rhode Island`, ` South Carolina`, ` South Dakota`, ` Tennessee`, ` Texas`, ` Utah`, ` Vermont`, ` Virginia`, ` Washington`, ` West Virginia`, ` Wisconsin`, ` Wyoming`)
+	fmt.Println(len(xs))
+	fmt.Println(cap(xs))
 
 	//using for range
-	for i, v := range slice {
+	for i, v := range xs {
 		fmt.Printf("%v - %v\n", i, v)
 	}
+	//without range
+	// for i := 0; i < len(xs); i++ {
+	// 	fmt.Println(xs[i], i)
+	// }
 }
