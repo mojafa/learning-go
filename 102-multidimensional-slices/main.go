b5ef47e0d557ca67b86f7fd6c7f1e1952c0e5f7d
mojafa <jmohsa@gmail.com>
2023-06-18 13:31:02 +0300

    multidimensional slices

---
+++ 102-multidimensional-slices/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	jb := []string{"james", "bond", "chocolate", "martini"}
+	fmt.Println(jb)
+	jm := []string{"jenny", "money", "cash", "martini"}
+	fmt.Println(jm)
+
+	xp := [][]string{jb, jm}
+	fmt.Println(xp)
+}
