052c59230249fdb9da3ff54627c019718759ebf6
mojafa <jmohsa@gmail.com>
2023-07-03 16:44:54 +0300

    defer

--- 134-defer/main.go
+++ 134-defer/main.go
@@ -1,5 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	defer foo()
+	bar()
+
+}
+
+// func (r receiver) identifier(p parameter(s)) (r return(s)) { code }
+
+func foo() {
+	fmt.Println("foo")
+}
+
+func bar() {
+	fmt.Println("bar")
 }
