35791f4ee5cab9785d64d0c01590dc867f1ffaa4
mojafa <jmohsa@gmail.com>
2023-07-03 16:45:12 +0300

    interfaces

--- 136-interfaces-polymorph/main.go
+++ 136-interfaces-polymorph/main.go
@@ -1,5 +1,48 @@
 package main
 
-func main() {
-	
+import "fmt"
+
+type person struct {
+	first string
 }
+
+type secretAgent struct {
+	person
+	ltk bool
+}
+
+func (p person) speak() {
+	fmt.Println("I am", p.first)
+}
+
+func (sa secretAgent) speak() {
+	fmt.Println("i am a secret agent", sa.first)
+}
+
+type human interface {
+	speak()
+}
+
+func saySomething(h human) {
+	h.speak()
+}
+
+func main() {
+	sa1 := secretAgent{
+		person: person{
+			first: "james",
+		},
+		ltk: true,
+	}
+
+	p2 := person{
+		first: "Jenny",
+	}
+	// sa1.speak()
+	// p2.speak()
+
+	saySomething(sa1)
+	saySomething(p2)
+}
+
+// func (r receiver) identifier(p parameter(s)) (return(s)) { code }
