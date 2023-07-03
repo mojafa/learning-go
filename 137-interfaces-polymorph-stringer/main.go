ce3b2fe0b8c5b9fdfd3deec48522f4e9ccee7ca4
mojafa <jmohsa@gmail.com>
2023-07-03 16:49:17 +0300

    interfaces

--- 137-interfaces-polymorph-stringer/main.go
+++ 137-interfaces-polymorph-stringer/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
-	
+import (
+	"fmt"
+	"strconv"
+)
+
+type book struct {
+	title string
 }
+
+func (b book) String() string {
+	return fmt.Sprint("the title of the book is ", b.title)
+}
+
+type count int
+
+func (c count) String() string {
+	return fmt.Sprint("this is the number ", strconv.Itoa(int(c)))
+}
+func main() {
+	b := book{
+		title: "West With the Night",
+	}
+	var n count = 42
+
+	fmt.Println(b)
+	fmt.Println(n)
+
+}
+
+// func (r receiver) identifier(p parameter(s)) (return(s)) { code }
