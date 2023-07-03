4e4916fffbc2c409da90905af0e75a4cc645b573
mojafa <jmohsa@gmail.com>
2023-07-03 16:44:27 +0300

    func syntax

--- 131-func-syntax/main.go
+++ 131-func-syntax/main.go
@@ -1,5 +1,45 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	foo()
+
+	bar("James")
+
+	s := aloha("Bond")
+	fmt.Println(s)
+
+	s1, n := dogYears("Bond", 40)
+	fmt.Println(s1, n)
+
 }
+
+func foo() {
+	fmt.Println("Hello from foo")
+}
+
+func bar(s string) {
+	fmt.Println("My name is", s)
+}
+
+func aloha(s string) string {
+	return fmt.Sprint("They call me Mr. ", s)
+}
+
+func dogYears(name string, age int) (string, int) {
+	age *= 7
+	return fmt.Sprint(name, " is this old in dog years "), age
+}
+
+// func (r receiver) identifier(p parameter(s)) (return(s)) { <your code here> }
+
+/*
+
+func syntax
+
+no params, no returns
+1 param, no returns
+1 param, 1 return
+2 params, 2 returns
+*/
