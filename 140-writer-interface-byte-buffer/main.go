4a63ad5a2e28b0637f83ce902a0e6a8455019bd4
mojafa <jmohsa@gmail.com>
2023-07-03 16:54:47 +0300

    writer interfaces

--- 140-writer-interface-byte-buffer/main.go
+++ 140-writer-interface-byte-buffer/main.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"bytes"
+	"fmt"
+)
+
+// type person struct {
+// 	first string
+// }
+
+// func (p person) writeOut(w io.Writer) error {
+// 	_, err := w.Write([]byte(p.first))
+// 	return err
+// }
+
 func main() {
-	
+	// f, err := os.Create("output.txt")
+	// if err != nil {
+	// 	log.Fatalf("error %s", err)
+	// }
+	// defer f.Close()
+
+	// s := []byte("Hello gophers!")
+
+	// _, err = f.Write(s)
+	// if err != nil {
+	// 	log.Fatalf("error %s", err)
+	// }
+
+	b := bytes.NewBufferString("Hello ")
+	fmt.Println(b.String())
+	b.WriteString("Gophers!")
+	fmt.Println(b.String())
+	b.Reset()
+	b.WriteString("It's Monday")
+	fmt.Println(b.String())
+
+	b.Write([]byte("It's Monday"))
+	fmt.Println(b.String())
 }
