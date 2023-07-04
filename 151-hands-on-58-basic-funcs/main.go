334edc9ab7020d880f446686696a828ddde75a4f
mojafa <jmohsa@gmail.com>
2023-07-04 09:39:36 +0300

    151-hands-on-58-basic-funcs/

--- 151-hands-on-58-basic-funcs/main.go
+++ 151-hands-on-58-basic-funcs/main.go
@@ -1,5 +1,27 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	x := foo()
+	fmt.Println(x)
+
+	i, y := bar()
+	fmt.Println(i, y)
+
 }
+
+func foo() int {
+	return 42
+}
+
+func bar() (int, string) {
+	return 42, "Jafa"
+}
+
+/*
+create a func with the identifier foo that returns an int
+create a func with the identifier bar that returns an int and a string
+call both funcs
+print out their results
+*/
