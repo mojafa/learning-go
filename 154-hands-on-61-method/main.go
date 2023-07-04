d6196be230466448b13621914486c60e93ca3e17
mojafa <jmohsa@gmail.com>
2023-07-04 09:40:44 +0300

    exercises

--- 154-hands-on-61-method/main.go
+++ 154-hands-on-61-method/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
-	
+import "fmt"
+
+type person struct {
+	first string
+	age   int
 }
+
+func (p person) speak() {
+	fmt.Println("I am", p.first, "and i'm ", p.age, "years old")
+}
+func main() {
+	p1 := person{
+		first: "James",
+		age:   30,
+	}
+	p2 := person{
+		first: "Jenny",
+		age:   30,
+	}
+	p1.speak()
+	p2.speak()
+
+}
+
+/*
+Create a user defined struct with
+the identifier “person”
+the fields:
+first
+age
+attach a method to type person with
+the identifier “speak”
+the method should have the person say their name and age
+create a value of type person
+call the method from the value of type person
+*/
--- 155-hands-on-62-interfaces/main.go
+++ 155-hands-on-62-interfaces/main.go
@@ -1,5 +1,63 @@
 package main
 
-func main() {
-	
+import (
+	"fmt"
+	"math"
+)
+
+type circle struct {
+	radius float64
 }
+
+type square struct {
+	length float64
+	width  float64
+}
+
+func (c circle) area() float64 {
+	return math.Pi * math.Pow(c.radius, 2)
+}
+
+func (s square) area() float64 {
+	return s.length * s.width
+}
+
+type shape interface {
+	area() float64
+}
+
+// create a func INFO which takes type shape and then prints the area
+func info(s shape) float64 {
+	return s.area()
+}
+
+func main() {
+	c1 := circle{
+		radius: 4,
+	}
+	s1 := square{
+		length: 5,
+		width:  5,
+	}
+	fmt.Println(info(c1))
+	fmt.Println(info(s1))
+
+}
+
+/*
+create a type SQUARE
+length int
+width int
+create a type CIRCLE
+radius float64
+attach a method to each that calculates AREA and returns it
+circle area= π r 2
+math.Pi
+math.Pow
+square area = L * W
+create a type SHAPE that defines an interface as anything that has the AREA method
+create a func INFO which takes type shape and then prints the area
+create a value of type square
+create a value of type circle
+use func info to print the area of square
+use func info to print the area of circle*/
