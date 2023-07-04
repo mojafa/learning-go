8d06674a898801834ff93670d2666a5ef96661a4
mojafa <jmohsa@gmail.com>
2023-07-04 09:37:50 +0300

    func expression

--- 143-func-expression/main.go
+++ 143-func-expression/main.go
@@ -1,5 +1,27 @@
 package main
 
+import "fmt"
+
 func main() {
+	foo()
+	x := func() {
+		fmt.Println("anonymous func ran")
+	}
+
+	y := func(s string) {
+		fmt.Println("This is an anonymous func showing my name", s)
+	}
+
+	x()
+	y("Jafa")
 
 }
+func foo() {
+	fmt.Println("Foo ran")
+}
+
+// a named function with an identifier
+// func (r receive) identifier(p parameter(s)) (r return(s)) { code }
+
+// an anonymous function
+// func(p parameter(s)) (r return(s)) { code }
