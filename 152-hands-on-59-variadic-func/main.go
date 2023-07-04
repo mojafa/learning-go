c4c36719e6a8420b5681ad57ee833e7905d2b5f4
mojafa <jmohsa@gmail.com>
2023-07-04 09:40:15 +0300

    variadic, defer

--- 152-hands-on-59-variadic-func/main.go
+++ 152-hands-on-59-variadic-func/main.go
@@ -1,5 +1,44 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+
+	xi := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+	fmt.Println(foo(xi...))
+	fmt.Println(bar(xi))
 }
+
+// func (r receiver) identifier(p paramaters) (return(s)) {code}
+
+func foo(ii ...int) int {
+	fmt.Println(ii)
+	fmt.Printf("%T\n", ii)
+
+	n := 0
+	for _, v := range ii {
+		n += v
+	}
+	return n
+}
+
+func bar(ii []int) int {
+	t := 0
+	for _, v := range ii {
+		t += v
+	}
+	return t
+}
+
+/*
+create a func with the identifier foo that
+takes in a variadic parameter of type int
+pass in a value of type []int into your func (unfurl the []int)
+returns the sum of all values of type int passed in
+
+create a func with the identifier bar that
+takes in a parameter of type []int
+returns the sum of all values of type int passed in
+
+*/
--- 153-hands-on-60-defer-func/main.go
+++ 153-hands-on-60-defer-func/main.go
@@ -1,5 +1,36 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	defer foo()
+	bar()
+	defer foo()
+
+	bar()
+	defer fmt.Println(3)
+	defer fmt.Println(2)
+	defer fmt.Println(1)
+	fmt.Println(0)
 }
+
+// func (r receiver) identifier(p parameter(s)) (r return(s)) { code }
+
+func foo() {
+	fmt.Println("foo")
+}
+
+func bar() {
+	fmt.Println("bar")
+}
+
+/*
+
+“defer” multiple functions in main
+show that a deferred func runs after the func containing it exits.
+determine the order in which the multiple defer funcs run
+
+*/
+
+// deferred functions run in LIFO order
+// last in first out LIFO
