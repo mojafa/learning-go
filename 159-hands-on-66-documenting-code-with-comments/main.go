f13bb193665ddfa7bbd2e570666a6919f7088d18
mojafa <jmohsa@gmail.com>
2023-08-01 22:54:15 +0300

    added main file content

--- 159-hands-on-66-documenting-code-with-comments/main.go
+++ 159-hands-on-66-documenting-code-with-comments/main.go
@@ -1,5 +1,12 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	fmt.Println(name("Jafa"))
+}
+
+// Name prints teh user's name as
+func name(s string) string {
+	return fmt.Sprint("My name is ", s)
 }
