af8f79157d0935641ad2cd04146ad67b96abc53b
mojafa <jmohsa@gmail.com>
2023-08-01 23:04:39 +0300

    func expressions

--- 162-hands-on-69-func-expression/main.go
+++ 162-hands-on-69-func-expression/main.go
@@ -1,5 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	y()
+	x := func() {
+		for i := 0; i < 10; i++ {
+			fmt.Println(i)
+		}
+	}
+	x()
+}
+
+var y = func() {
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
 }
