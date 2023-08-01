a89ac43dcff23a6d38e007ffa4d9c6e414ee4d21
mojafa <jmohsa@gmail.com>
2023-08-01 23:23:43 +0300

    closures

--- 165-hands-on-72-closure/main.go
+++ 165-hands-on-72-closure/main.go
@@ -1,5 +1,8 @@
 package main
 
 func main() {
-	
+
 }
+
+// A closure is a function value that references variables from outside its body.
+// The function may access and assign to the referenced variables; in this sense the function is "bound" to the variables.
