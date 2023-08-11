774f33d37bf6270e9288845f4711a422a4b1db3c
mojafa <jmohsa@gmail.com>
2023-08-11 22:01:59 +0300

    closure

--- 165-hands-on-72-closure/main.go
+++ 165-hands-on-72-closure/main.go
@@ -1,7 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 func main() {
 
+	x := power(2)
+	fmt.Println(x())
+	fmt.Println(x())
+	fmt.Println(x())
+	fmt.Println(x())
+	fmt.Println(x())
+	fmt.Println(x())
+
+}
+
+func power(a float64) func() float64 {
+	var c float64
+	return func() float64 {
+		c++
+		return math.Pow(a, c)
+	}
 }
 
 // A closure is a function value that references variables from outside its body.
