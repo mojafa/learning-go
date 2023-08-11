487859df76032d5aa5489493880038be0e77c852
mojafa <jmohsa@gmail.com>
2023-08-11 22:02:07 +0300

    wrapper

--- 166-hands-on-73-wrapper/main.go
+++ 166-hands-on-73-wrapper/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
-	
+	timeFunc(dowork)
+
+}
+
+func dowork() {
+	for i := 0; i < 2000; i++ {
+		fmt.Println(i)
+	}
+}
+
+func timeFunc(f func()) {
+	start := time.Now()
+	f()
+	elapsed := time.Since(start)
+	fmt.Println(elapsed)
 }
