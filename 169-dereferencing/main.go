067505afab6ef835473f52ff202375c613e52375
mojafa <jmohsa@gmail.com>
2023-08-12 09:38:43 +0300

    dereferencing

--- 169-dereferencing/main.go
+++ 169-dereferencing/main.go
@@ -1,5 +1,21 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	x := 42
+	fmt.Println(x)
+	fmt.Println(&x)
+
+	y := &x
+	fmt.Println(y)
+	fmt.Printf("%v\t%T\n", y, y)
+
+	fmt.Println(*y)
+	fmt.Println(*&x)
+
+	*y = 43
+	fmt.Println(x)
+	fmt.Println(*y)
+
 }
