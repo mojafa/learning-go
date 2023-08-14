e6f79f9ef38ae6d869e206a5ee6b7373658c537a
mojafa <jmohsa@gmail.com>
2023-08-14 20:29:08 +0300

    generics and some back to basics codes

--- 001-numeric-consts/main.go
+++ 001-numeric-consts/main.go
@@ -11,7 +11,7 @@ func needInt(x int) int { return x*10 + 1 }
 
 func needFloat(x float64) float64 { return x * 0.1 }
 func main() {
-	fmt.Println(needInt(Small))
+	// fmt.Println(needInt(Big))
 	fmt.Println(needFloat(Small))
 	fmt.Println(needFloat(Big))
 }
