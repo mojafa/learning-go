4009f6e6a37a093d4f4523031b017876024a2c6c
mojafa <jmohsa@gmail.com>
2023-08-21 17:02:05 +0300

    range;'

---
+++ go-fundamentals/channels/range-2/main.go
@@ -0,0 +1,29 @@
+// ● close a channel
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	c := make(chan int)
+
+	go send(c)
+
+	receive(c)
+
+	close(c) // WE CLOSED THE CHANNEL
+
+	fmt.Println("about to exit")
+}
+
+// send channel
+func send(c chan<- int) {
+	c <- 42
+}
+
+// receive channel
+func receive(c <-chan int) {
+	fmt.Println("the value received from the channel:", <-c)
+}
