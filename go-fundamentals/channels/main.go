4ebc9a6b5c95c71e51563175a0db0c301ad78794
mojafa <jmohsa@gmail.com>
2023-08-21 17:01:03 +0300

    channels revisited

--- go-fundamentals/channels/main.go
+++ go-fundamentals/channels/main.go
@@ -47,3 +47,51 @@ func main (){
 }
 
 */
+
+/*
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	c := make(chan int)
+	cr := make(<-chan int) // receive
+	cs := make(chan<- int) // send
+
+	fmt.Println("-----")
+	fmt.Printf("c\t%T\n", c)
+	fmt.Printf("cr\t%T\n", cr)
+	fmt.Printf("cs\t%T\n", cs)
+
+	// specific to general doesn't assign
+	c = cr
+	c = cs
+}
+*/
+
+/*
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	c := make(chan int)
+	cr := make(<-chan int) // receive
+	cs := make(chan<- int) // send
+
+	fmt.Println("-----")
+	fmt.Printf("c\t%T\n", c)
+	fmt.Printf("cr\t%T\n", cr)
+	fmt.Printf("cs\t%T\n", cs)
+
+	// general to specific converts
+	fmt.Println("-----")
+	fmt.Printf("c\t%T\n", (<-chan int)(c))
+	fmt.Printf("c\t%T\n", (chan<- int)(c))
+
+}
+*/
