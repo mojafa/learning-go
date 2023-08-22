9f3f991ecd8a2cd3f1c788bec45dd9472911a469
mojafa <jmohsa@gmail.com>
2023-08-22 18:11:31 +0300

    setting-app-wide-config/

---
+++ building-web-app-in-go-fundamentals/setting-app-wide-config/cmd/web/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/mojafa/building-web-app-in-go-fundamentals/setting-app-wide-config/pkg/config"
+	"github.com/mojafa/building-web-app-in-go-fundamentals/setting-app-wide-config/pkg/handlers"
+	"github.com/mojafa/building-web-app-in-go-fundamentals/setting-app-wide-config/pkg/render"
+)
+
+const portNumber = ":8080"
+
+// main is the main function
+func main() {
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
+
+	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
+}
---
+++ building-web-app-in-go-fundamentals/setting-app-wide-config/pkg/handlers/handlers.go
@@ -0,0 +1,17 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/mojafa/building-web-app-in-go-fundamentals/more-complex-cache/pkg/render"
+)
+
+// Home is the handler for the home page
+func Home(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "home.page.tmpl")
+}
+
+// About is the handler for the about page
+func About(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "about.page.tmpl")
+}
