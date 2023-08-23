fb81b216996a15dbf901056e4752488b9eab60ab
mojafa <jmohsa@gmail.com>
2023-08-23 14:40:46 +0300

    Sharing data with templates

--- building-web-app-in-go-fundamentals/sharing-data-with-templates/pkg/handlers/handlers.go
+++ building-web-app-in-go-fundamentals/sharing-data-with-templates/pkg/handlers/handlers.go
@@ -3,30 +3,44 @@ package handlers
 import (
 	"net/http"
 
-	"github.com/mojafa/building-web-app-in-go-fundamentals/optmizing-cache-using-application-config/pkg/config"
-	"github.com/mojafa/building-web-app-in-go-fundamentals/optmizing-cache-using-application-config/pkg/render"
+	"github.com/mojafa/building-web-app-in-go-fundamentals/sharing-data-with-templates/pkg/config"
+	"github.com/mojafa/building-web-app-in-go-fundamentals/sharing-data-with-templates/pkg/models"
+	"github.com/mojafa/building-web-app-in-go-fundamentals/sharing-data-with-templates/pkg/render"
 )
 
+// Repo the repository used by the handlers
 var Repo *Repository
 
+// Repository is the repository type
 type Repository struct {
 	App *config.AppConfig
 }
 
+// NewRepo creates a new repository
 func NewRepo(a *config.AppConfig) *Repository {
 	return &Repository{
 		App: a,
 	}
 }
 
+// NewHandlers sets the repository for the handlers
 func NewHandlers(r *Repository) {
 	Repo = r
 }
 
+// Home is the handler for the home page
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "home.page.tmpl")
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
 
+// About is the handler for the about page
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "about.page.tmpl")
+	// perform some logic
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again"
+
+	// send data to the template
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
 }
