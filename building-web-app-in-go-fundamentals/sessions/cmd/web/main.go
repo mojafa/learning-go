9d491e957d9ad57323b9a5aa5e30ae3a8173f8e6
mojafa <jmohsa@gmail.com>
2023-08-31 20:50:45 +0300

    go mod tidy

---
+++ building-web-app-in-go-fundamentals/sessions/cmd/web/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/mojafa/building-web-app-in-go-fundamentals/sessions/pkg/handlers"
+	"github.com/mojafa/building-web-app-in-go-fundamentals/sessions/pkg/render"
+)
+
+const portNumber = ":8080"
+
+// main is the main function
+func main() {
+	// change this to true when in production
+	app.InProduction = false
+
+	//set up the session
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,14 +1,5 @@
-module github.com/mojafa
+module github.com/learning-go
 
 go 1.20
 
-require (
-	github.com/GoesToEleven/dog v0.0.0-20230428023317-90bef1c76cb9 // indirect
-	github.com/GoesToEleven/puppy v1.2.0
-	github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f
-	github.com/go-chi/chi v1.5.4
-	github.com/justinas/nosurf v1.1.1
-	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3
-	golang.org/x/exp v0.0.0-20230811145659-89c5cff77bcb
-	golang.org/x/sys v0.1.0 // indirect
-)
+require github.com/alexedwards/scs/v2 v2.5.1
